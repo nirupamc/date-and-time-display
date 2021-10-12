fca616725fa7c7ef4de6da3f0f585c5159e26cff
Nirupam Changmai <73378284+nirupamc@users.noreply.github.com>
2021-10-12 14:39:00 +0530

    first commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
+
+const fName = 'Nirupam';
+const sName = 'Changmai';
+const date = new Date().toDateString();
+const time = new Date().toTimeString();
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <>
+    <h1>{`My name is ${fName} ${sName}`}</h1>
+    <p> today Date is {date}</p>
+    <p>and the time is - {time} </p>
+  </>,
   document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
